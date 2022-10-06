cec069281a564e000e27fce2e56925ed2a84b500
ksh940925 <ksh9409255@naver.com>
2022-10-06 09:47:50 +0900

    feat: init setting

---
+++ src/main/java/com/team6/onandthefarmsnsservice/OnandthefarmSnsserviceApplication.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmsnsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnandthefarmSnsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnandthefarmSnsserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/onandthefarmsnsservice/OnandthefarmSnsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmsnsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnandthefarmSnsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
