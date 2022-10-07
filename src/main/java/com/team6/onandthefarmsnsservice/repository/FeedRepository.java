144f2a12b55ff8e2c30cfbcd374651b783259f42
kanamycine <jinyeong5320@gmail.com>
2022-10-07 15:09:00 +0900

    feat : 프로필 메인 화면 feed 부분 조회 기능 구현

--- src/main/java/com/team6/onandthefarmsnsservice/repository/FeedRepository.java
+++ src/main/java/com/team6/onandthefarmsnsservice/repository/FeedRepository.java
@@ -3,6 +3,7 @@ package com.team6.onandthefarmsnsservice.repository;
 import com.team6.onandthefarmsnsservice.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,9 @@ import java.util.Optional;
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
     Feed findByMemberId(Long memberId);
+
+    @Query("select f from Feed f where f.memberId =:memberId")
+    List<Feed> findFeedListByMemberId(@Param("memberId") Long memberId);
 
     @Query("select f from Feed f where f.feedId=:feedId and f.feedStatus=:true")
     Optional<Feed> findByIdAndStatus(Long feedId);
--- src/main/java/com/team6/onandthefarmsnsservice/service/FeedService.java
+++ src/main/java/com/team6/onandthefarmsnsservice/service/FeedService.java
@@ -2,9 +2,13 @@ package com.team6.onandthefarmsnsservice.service;
 
 
 import com.team6.onandthefarmsnsservice.dto.FeedDto;
+import com.team6.onandthefarmsnsservice.dto.profile.ProfileMainFeedDto;
 import com.team6.onandthefarmsnsservice.vo.FeedDetailResponse;
 import com.team6.onandthefarmsnsservice.vo.FeedResponse;
 import com.team6.onandthefarmsnsservice.dto.FeedInfoDto;
+import com.team6.onandthefarmsnsservice.vo.profile.ProfileMainFeedRequest;
+import com.team6.onandthefarmsnsservice.vo.profile.ProfileMainFeedResponse;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -26,6 +30,9 @@ public interface FeedService {
     Long uploadFeed(Long memberId, String memberRole, FeedInfoDto feedInfoDto);
 
     FeedDetailResponse findFeedDetail(Long feedId);
+
     Boolean upViewCount(Long feedId);
+
+    List<ProfileMainFeedResponse> findByMemberFeedList(ProfileMainFeedDto profileMainFeedDto);
     
 }
