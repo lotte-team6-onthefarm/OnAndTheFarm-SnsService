be8d73c2057374a366ac3aeccbea9267b693c031
kanamycine <jinyeong5320@gmail.com>
2022-10-28 09:35:59 +0900

    feat : sns msa 분리 sns profile - 진영 담당 메서드

---
+++ src/main/java/com/team6/onandthefarmsnsservice/vo/product/WishVo.java
@@ -0,0 +1,24 @@
+package com.team6.onandthefarmsnsservice.vo.product;
+
+import com.team6.onandthefarmsnsservice.vo.user.UserVo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class WishVo {
+	private Long wishId;
+
+	private Long productId;
+
+	private UserVo user;
+
+	private Boolean wishStatus;
+}
